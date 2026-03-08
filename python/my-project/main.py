54a3db91d144278f8d86eb5926756d98790b627e
SedjamTan <62494738+SedjamTan@users.noreply.github.com>
2026-03-08 15:44:31 +0800

    added initial function plotting

--- python/my-project/main.py
+++ python/my-project/main.py
@@ -1,16 +1,26 @@
 from manim import * 
 class axes(Scene):
     def construct(self):
-        axes = Axes(x_range = (-1, 12), y_range = (-1, 6), tips = False)
+        axes = Axes(
+            x_range = (1903, 2024, 11), 
+            y_range = (0, 4000000, 500000), 
+            tips = False,
+            x_axis_config={
+                "decimal_number_config": {
+                    "group_with_commas": False,
+                    "num_decimal_places": 0
+                }
+            }
+            )
+        axes.shift(RIGHT * 0.7)
         axes.add_coordinates()
-        x_lab = axes.get_x_axis_label("X axis")
-        y_lab = axes.get_y_axis_label("Y axis")
+        x_lab = axes.get_x_axis_label("Year")
+        y_lab = axes.get_y_axis_label("Population")
 
         self.play(Write(axes), Write(x_lab), Write(y_lab))
-        
-        rec = Rectangle(height = 0.5, width = 0.5) # this thing just draws somewhere in the plot
-        rec.move_to(axes.c2p(4, 2))
 
-        self.play(Write(rec))
+        f1 = axes.plot(lambda x: ((12.327801029757717)*((x-1903)**3))+((-9.393051541049447*10**2)*((x-1903)**2))+((2.627648874654535*10**4)*(x-1903))+15559,x_range=[1903,1918], color = BLUE)
+
+        self.play(Write(f1))
         self.wait(2)
     # use manim -pqh main.py
