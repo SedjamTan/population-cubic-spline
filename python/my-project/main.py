17c4539ec35d6cc6d5ee3b7346f2faa8a9845739
SedjamTan <62494738+SedjamTan@users.noreply.github.com>
2026-03-20 20:33:53 +0800

    Remove trailing whitespace, rename classes to PascalCase, and replace unnecessary MathTex() with Tex()
    - removed trailing whitespace
    - renamed the classes to follow PascalCase
    - converted some MathTex() to Tex() since some of them has no math in it

--- python/my-project/main.py
+++ python/my-project/main.py
@@ -1,12 +1,12 @@
 """main script of manim"""
 
 import math_functions
-from manim import * 
+from manim import *
 import numpy
 
 # Typically use manim -qh main.py
 
-class population_graph(Scene):
+class PopulationGraph(Scene):
     def construct(self):
         years = [1903,1918,1939,1948,1960,1970,1975,1980,1990,1995,2000,2007,2010,2015,2020,2024]
         population = [15559,239969,319339,394642,514346,737975,
@@ -14,8 +14,8 @@ class population_graph(Scene):
                       2822216,2924433,3292071,3708890,3876806]
 
         axes = Axes(
-            x_range = (1903, 2024, 11), 
-            y_range = (0, 4000000, 500000), 
+            x_range = (1903, 2024, 11),
+            y_range = (0, 4000000, 500000),
             tips = False,
             x_length=config.frame_width-4,
             #axis_config={
@@ -30,6 +30,7 @@ class population_graph(Scene):
         )
 
         axes.add_coordinates()#(color=BLACK)
+
         x_lab = axes.get_x_axis_label("Year")
         y_lab = axes.get_y_axis_label("Population")
         #y_lab.color = x_lab.color = BLACK
@@ -60,7 +61,7 @@ class population_graph(Scene):
         interpolation_func = [f1_plot,f2_plot,f3_plot,f4_plot,f5_plot,
                               f6_plot,f7_plot,f8_plot,f9_plot,f10_plot,
                               f11_plot,f12_plot,f13_plot,f14_plot,f15_plot]
-            
+
         create_func = []
         for func in interpolation_func:
             create_func.append(Create(func))
@@ -68,11 +69,11 @@ class population_graph(Scene):
         self.play(create_func)
         self.wait(2)
 
-class population_growth(Scene):
+class PopulationGrowth(Scene):
     def construct(self):
         axes = Axes(
-            x_range = (1903, 2024, 11), 
-            y_range = (0, 100000, 10000), 
+            x_range = (1903, 2024, 11),
+            y_range = (0, 100000, 10000),
             tips = False,
             x_length=config.frame_width-4,
             #axis_config={
@@ -87,8 +88,9 @@ class population_growth(Scene):
         )
 
         axes.add_coordinates()#(color=BLACK)
+
         x_lab = axes.get_x_axis_label("Year")
-        y_lab = axes.get_y_axis_label(MathTex(r"\text{Population Growth Rate}"))
+        y_lab = axes.get_y_axis_label(Tex("Population Growth Rate"))
         #y_lab.color = x_lab.color = BLACK
 
         self.play(Write(axes), Write(x_lab), Write(y_lab))
@@ -108,7 +110,6 @@ class population_growth(Scene):
         f13p_plot = axes.plot(math_functions.f13p,x_range=[2010,2015],color = BLUE)
         f14p_plot = axes.plot(math_functions.f14p,x_range=[2015,2020],color = BLUE)
         f15p_plot = axes.plot(math_functions.f15p,x_range=[2020,2024],color = BLUE)
-
         first_derivative_func = [f1p_plot,f2p_plot,f3p_plot,f4p_plot,f5p_plot,
                                  f6p_plot,f7p_plot,f8p_plot,f9p_plot,f10p_plot,
                                  f11p_plot,f12p_plot,f13p_plot,f14p_plot,f15p_plot]
@@ -122,12 +123,12 @@ class population_growth(Scene):
         y = math_functions.f11p(x)
 
         peak_index = np.argmax(y)
-
         peak_x = x[peak_index]
         peak_y = y[peak_index]
-        point = axes.c2p(peak_x, peak_y)
 
+        point = axes.c2p(peak_x, peak_y)
         dot = Dot(point,color=YELLOW,z_index=2)
+
         title = f"\\text{{Growth rate:}} \\\\ \\text{{{peak_y:.0f} people/year at {peak_x:.2f}}}"
         label = MathTex(title,font_size=25).move_to(axes.c2p(peak_x,peak_y) + LEFT*2)
 
@@ -135,11 +136,11 @@ class population_growth(Scene):
         self.play(Create(label))
         self.wait(2)
 
-class population_accelerate(Scene):
+class PopulationAccelerate(Scene):
     def construct(self):
         axes = Axes(
-            x_range = (1903, 2024, 11), 
-            y_range = (-20000, 20000, 5000), 
+            x_range = (1903, 2024, 11),
+            y_range = (-20000, 20000, 5000),
             tips = False,
             x_length=config.frame_width-4,
             #axis_config={
@@ -155,7 +156,7 @@ class population_accelerate(Scene):
 
         axes.add_coordinates()#(color=BLACK)
         x_lab = axes.get_x_axis_label("Year")
-        y_lab = axes.get_y_axis_label(MathTex(r"\text{Population Growth Acceleration}"))
+        y_lab = axes.get_y_axis_label(Tex("Population Growth Acceleration"))
         #y_lab.color = x_lab.color = BLACK
 
         self.play(Write(axes), Write(x_lab), Write(y_lab))
@@ -175,10 +176,10 @@ class population_accelerate(Scene):
         f13pp_plot = axes.plot(math_functions.f13pp,x_range=[2010,2015],color = BLUE)
         f14pp_plot = axes.plot(math_functions.f14pp,x_range=[2015,2020],color = BLUE)
         f15pp_plot = axes.plot(math_functions.f15pp,x_range=[2020,2024],color = BLUE)
-
         second_derivative_func = [f1pp_plot,f2pp_plot,f3pp_plot,f4pp_plot,f5pp_plot,
                                   f6pp_plot,f7pp_plot,f8pp_plot,f9pp_plot,f10pp_plot,
                                   f11pp_plot,f12pp_plot,f13pp_plot,f14pp_plot,f15pp_plot]
+
         for func in second_derivative_func:
             self.play(Create(func), run_time=0.5)
 
@@ -188,13 +189,12 @@ class population_accelerate(Scene):
         y = math_functions.f13pp(x)
 
         peak_index = np.argmax(y)
-
         peak_x = x[peak_index]
         peak_y = y[peak_index]
 
         point = axes.c2p(peak_x, peak_y)
-
         dot = Dot(point,color=YELLOW,z_index=2)
+
         title = rf"""
                 \text{{Acceleration of Growth Rate:}} \\ 
                 \text{{{peak_y:.0f} people / year}}^2 
@@ -202,11 +202,12 @@ class population_accelerate(Scene):
                 """
 
         label = MathTex(title,font_size=25).move_to(axes.c2p(peak_x,peak_y) + LEFT*2)
+
         self.add(dot)
         self.play(Create(label))
         self.wait(2)
 
-class interpolate_example(Scene):
+class InterpolateExample(Scene):
     def construct(self):
         axes = Axes(
             x_range = (-8, 8, 1),
@@ -242,7 +243,7 @@ class interpolate_example(Scene):
 
         self.play(Create(f1_plot))
 
-class logistic_growth_model(Scene):
+class LogisticGrowthModel(Scene):
     def construct(self):
         equation = r"""P(t) = \frac{a}{1 + e^{-b(t-c)}}"""
         label = r"""\text{Logistic Growth Model}"""
@@ -265,26 +266,26 @@ class logistic_growth_model(Scene):
 
         self.play(Write(characteristics))
 
-class growth_functions(Scene):
+class GrowthFunctions(Scene):
     def construct(self):
         growth_rate = r"""P^{\prime}(t) = \frac{dP(t)}{dt}"""
         growth_accelerate = r"""P^{\prime\prime}(t) = \frac{d^2P(t)}{dt^2}"""
 
-        label_growth_rate = r"""\text{Growth Rate}"""
-        label_growth_accelerate = r"""\text{Growth Acceleration}"""
+        label_growth_rate = r"""Growth Rate"""
+        label_growth_accelerate = r"""Growth Acceleration"""
 
         eq1 = MathTex(growth_rate,font_size=100).move_to((UP*1.5))
 
         self.play(Write(eq1))
         self.play(eq1.animate.shift(LEFT * 3))
 
-        title1 = MathTex(label_growth_rate).move_to(eq1.get_right() + RIGHT*4)
+        title1 = Tex(label_growth_rate).move_to(eq1.get_right() + RIGHT*4)
 
         self.play(Write(title1))
         self.wait(1)
 
         eq2 = MathTex(growth_accelerate,font_size=100).next_to(eq1,DOWN)
-        title2 = MathTex(label_growth_accelerate).move_to(eq2.get_right() + RIGHT*4)
+        title2 = Tex(label_growth_accelerate).move_to(eq2.get_right() + RIGHT*4)
 
         self.play(Write(eq2))
         self.play(Write(title2))
